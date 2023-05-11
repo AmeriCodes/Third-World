bf70884a060249bf904f84a0cb3c942c9bde4a3c
Mateus Américo <mateus-americo@live.com>
2023-05-11 12:23:53 -0300

    ex 75 pela metade, tive que sair

--- Exercises/Ex075.py
+++ Exercises/Ex075.py
@@ -3,3 +3,9 @@
 # B) Em que posição foi digitado o primeiro valor 3.
 # C) Quais foram os números pares.
 
+number_1 = int(input('Digite um número: '))
+number_2 = int(input('Digite outro número: '))
+number_3 = int(input('Digite mais um número: '))
+number_4 = int(input('Digite o último número: '))
+tupla = (number_1, number_2, number_3, number_4)
+print(tupla)
