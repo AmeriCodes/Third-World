c566851a681c3306c28ecb263240ec72e4ed59d6
Mateus Américo <mateus-americo@live.com>
2023-05-16 20:36:08 -0300

    Maior e menor valores na lista

--- Exercises/Ex078.py
+++ Exercises/Ex078.py
@@ -1,3 +1,26 @@
 # Faça um programa que leia 5 valores numéricos e guarde-os em uma lista.
 # No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista.
 
+lista = list()
+bigger = minor = 0
+
+for c in range(0,5):
+    lista.append(int(input(f'Digite um valor para a posição {c}: ')))
+    if c == 0:
+        bigger = minor = lista[c]
+    else:
+        if lista[c] > bigger:
+            bigger = lista[c]
+        if lista[c] < minor:
+            minor = lista[c]
+print('#=' * 20)
+print(f'Você digitou os valores {lista}')
+print(f'O maior valor digitado foi {bigger} nas posições ', end='')
+for i, v in enumerate(lista):
+    if v == bigger:
+        print(f'{i}...', end='')
+print()
+print(f'O menor valor digitado foi {minor} nas posições ', end='')
+for i, v in enumerate(lista):
+    if v == minor:
+        print(f'{i}...', end='')
