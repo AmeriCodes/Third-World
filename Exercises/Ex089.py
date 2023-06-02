9e476dcc3fccd2b925679f241f9598a753df5ed5
AmeriCodes <mateus-americo@live.com>
2023-06-01 21:30:30 -0300

    traduzindo o exercício

--- Exercises/Ex089.py
+++ Exercises/Ex089.py
@@ -1,25 +1,25 @@
 # Crie um programa que leia o nome e duas notas de vários alunos e guarde tudo em uma lista composta. No final, mostre um boletim contendo a média de cada um e permita que o usuário possa mostrar as notas de cada aluno indivdualmente.
 
-ficha = list()
+report_card = list()
 while True:
-    nome = str(input('Nome: ')).strip().title()
-    nota1 = float(input('Nota 1: '))
-    nota2 = float(input('Nota 2: '))
-    media = (nota1 + nota2) / 2
-    ficha.append([nome, [nota1, nota2], media])
-    pergunta = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
-    if pergunta == 'N':
+    name = str(input('Nome: ')).strip().title()
+    grade1 = float(input('Nota 1: '))
+    grade2 = float(input('Nota 2: '))
+    average = (grade1 + grade2) / 2
+    report_card.append([name, [grade1, grade2], average])
+    question = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if question == 'N':
         break
 print('-=' * 30)
 print(f'{"No.":<4}{"NOME":<18}{"MÉDIA":>8}')
 print('-' * 26)
-for i, a in enumerate(ficha):
-    print(f'{i:<4}{a[0]:<18}{a[2]:>8.1f}')
+for i, studant in enumerate(report_card):
+    print(f'{i:<4}{studant[0]:<18}{studant[2]:>8.1f}')
 while True:
     print('-' * 30)
-    pergunta_específica = int(input('Mostrar nostas de qual aluno? (999 interrompe): '))
-    if pergunta_específica == 999:
+    specific_question = int(input('Mostrar nostas de qual aluno? (999 interrompe): '))
+    if specific_question == 999:
         break
-    if pergunta_específica <= len(ficha) - 1:
-        print(f'Notas de {ficha[pergunta_específica][0]} são {ficha[pergunta_específica][1]}')
+    if specific_question <= len(report_card) - 1:
+        print(f'Notas de {report_card[specific_question][0]} são {report_card[specific_question][1]}')
 print('<<< VOLTE SEMPRE >>>')
