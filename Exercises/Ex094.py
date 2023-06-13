c3372f98565a6bf309b91c444cb6ab27b696375e
AmeriCodes <mateus-americo@live.com>
2023-06-13 16:07:03 -0300

    unindo listas e dicionários

--- Exercises/Ex094.py
+++ Exercises/Ex094.py
@@ -8,6 +8,7 @@ D) uma lista com idade acima da média
 """
 galera = list()
 pessoas = dict()
+soma = media = 0
 while True:
     pessoas['nome'] = str(input('Nome: '))
     while True:
@@ -16,6 +17,7 @@ while True:
             break
         print('ERRO! Por favor, digite M ou F.')
     pessoas['idade'] = int(input('Idade: '))
+    soma += pessoas['idade']
     galera.append(pessoas.copy())
     while True:
         continuar = str(input('Deseja continuar? [S/N]')).upper()[0]
@@ -24,4 +26,20 @@ while True:
         print('ERRO! Responda apenas S ou N.').upper()[0]
     if continuar == 'N':
         break
-print(galera)
+print(f'{"ANALISE DE DADOS":-^40}')
+print(f'A) - {len(galera)} pessoas foram cadastradas')
+media = soma / len(galera)
+print(f'B) - A media de idade é de {media:5.2f} anos.')
+print('C) - As mulheres cadastradas foram, ' , end='')
+for p in galera:
+    if p['sexo'] in 'Ff':
+        print(f'{p["nome"]}, ', end='')
+print()
+print('D) - Lista das pessoas acima da média de idade: ')
+for p in galera:
+    if p['idade'] >= media:
+        print('     ', end='')
+        for k,v in p.items():
+            print(f'{k} = {v}; ', end='')
+        print()
+print(f'{"Encerrado":-^40}')
