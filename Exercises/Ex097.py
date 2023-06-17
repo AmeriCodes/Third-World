da51b22c67afa4ec4765aea673506b79219c913e
AmeriCodes <mateus-americo@live.com>
2023-06-17 13:17:25 -0300

    print com mensagem e formatação de tamanho variável

--- Exercises/Ex097.py
+++ Exercises/Ex097.py
@@ -9,3 +9,16 @@ saída
 Olá, Mundo!
 ~~~~~~~~~~~~
 """
+
+
+
+def mensagem_variavel(msg):
+    tamanho_da_mensagem = len(msg) +4
+    print(f'{"=" * tamanho_da_mensagem}')
+    print(f'  {msg}')
+    print(f'{"=" * tamanho_da_mensagem}')
+
+
+mensagem_variavel('testando')
+mensagem_variavel('Américo')
+mensagem_variavel("I'm learning English!")
