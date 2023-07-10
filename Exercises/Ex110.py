538f2350217adae895e9b983e064e997225ff0e3
AmeriCodes <mateus-americo@live.com>
2023-07-10 11:23:44 -0300

    usando pacotes

--- Exercises/Ex110.py
+++ Exercises/Ex110.py
@@ -1,3 +1,7 @@
+"""
+Adicione ao módulo criado nos exercícios anteriores, uma função hamada resumo(), que mostre na tela algumas informações geradas pelas funções que já temos no módulo criado até aqui.
+"""
+
 from practicus import numeri
 
 valor = float(input('Digite o preço: R$ '))
---
+++ Exercises/Ex111.py
@@ -0,0 +1,8 @@
+"""
+Crie um pacote chamado utilidadesCeV que tenha dois módulos internos chamados moeda e dado.
+Transfira todas as funções utilizadas nos ex 107, 108, 109 para o primeiro pacote e mantenha tudo funcionando.
+"""
+from practicus import numeri
+
+valor = float(input('Digite o valor: '))
+numeri.resumo(valor, 30, 10)
